61dfe9f21e8aa60e6f04f5503ef599019a769b4b
Dmitriy <dmitr.wischnewski@mail.ru>
2023-02-04 12:44:20 +0300

    Masha in a house!

---
+++ ex5MASHA/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите имя пользователя: ");
+string userName = Console.ReadLine()!;
+if(userName.ToLower() == "маша")
+{
+    Console.WriteLine("Дратуйтэ, Маша!");
+}
+else
+{
+     Console.WriteLine("Доброго, " + userName + "!");
+}
