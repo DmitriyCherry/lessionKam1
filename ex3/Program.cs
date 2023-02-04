953ca686553dfd3369a242020abe8d6a33810220
Dmitriy <dmitr.wischnewski@mail.ru>
2023-02-04 12:01:17 +0300

    ex3 is live!

---
+++ ex3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите первое число: ");
+int firstValue = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondValue = int.Parse(Console.ReadLine());
+int sumResult = firstValue + secondValue;
+Console.Write("Сумма чисел: " + sumResult);
