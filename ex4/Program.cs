ee8bfbbf1fa7fcffb6da53d6afa8a364372737c5
Dmitriy <dmitr.wischnewski@mail.ru>
2023-02-04 12:12:07 +0300

    ex4 is live!

---
+++ ex4/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите первое число: ");
+double firstValue = double.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+double secondValue = double.Parse(Console.ReadLine()!);
+double divResult = firstValue / secondValue;
+Console.Write("Результат деления двух чисел: " + divResult);
